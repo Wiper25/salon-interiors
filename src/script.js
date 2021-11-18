84bf232db49ded65e68ef6d36d07a9189ae0cc00
wiper <wiper996100@gmail.com>
2021-11-18 12:40:26 +0600

    Добавил модельное окно

--- src/script.js
+++ src/script.js
@@ -1,5 +1,9 @@
-$(document).ready(function(){
-    $('.your-class').slick({
-      setting-name: setting-value
-    });
-  });
+let btnBurgerMenu = document.querySelector('.mobile-burger-menu').onclick = () => {
+    burgerMenu.classList.toggle('display')
+}
+btnModelWin = document.querySelector('.btn-application').onclick = () => {
+    modelWin.classList.toggle('display')
+    burgerMenu.classList.toggle('display')
+}
+burgerMenu = document.querySelector('.model-burger-menu')
+modelWin = document.querySelector('.model-window-form-application')
